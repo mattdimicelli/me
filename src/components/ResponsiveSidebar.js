06a41c62437e07c9fd3ff80fa8cd2eab5d8dd1e2
Matt Di Micelli <matthewdimicelli@gmail.com>
2022-02-05 01:14:56 -0500

    complete

--- src/components/ResponsiveSidebar.js
+++ src/components/ResponsiveSidebar.js
@@ -1,6 +1,68 @@
 import profilePic from '../images/profile_pic_oval.webp';
+import { Link } from 'react-scroll';
+import { useState, useEffect } from 'react';
 
 const ResponsiveSidebar = ({showSidebar}) => {
+    let [activeAbout, setActiveAbout] = useState(false);
+    let [activeSkills, setActiveSkills] = useState(false);
+    let [activeProjects, setActiveProjects] = useState(false);
+    let [activeEducation, setActiveEducation] = useState(false);
+    let [activeInterests, setActiveInterests] = useState(false);
+
+    useEffect(() => {
+        const ioOptions = {
+            threshold: 0.5,
+        }
+        const ioOptionsForProjectEl = {
+            threshold: 0.2,
+        }
+        let educationActive;
+        let projectsActive;
+
+        const ioCallback = (entries, observer) => {
+            entries.forEach(entry => {
+                // eslint-disable-next-line default-case
+                switch (entry.target.id) {
+                    case 'about':
+                        setActiveAbout(entry.isIntersecting);
+                        break;
+                    case 'skills':
+                        setActiveSkills(entry.isIntersecting);
+                        break;
+                    // Since the projects section is longer than the other,
+                    // at certain points the threshold might be met for both
+                    // the projects and education sections.  To prevent both 
+                    // links from turning white at same time, extra variables
+                    // are used (the React state cannot be used for this bc
+                    // it is not updated immediately d/t async nature)
+                    case 'projects':
+                       if (educationActive) break;
+                       setActiveProjects(entry.isIntersecting);
+                       projectsActive = entry.isIntersecting;
+                       break;
+                    case 'education':
+                        if (projectsActive) break;
+                        setActiveEducation(entry.isIntersecting);
+                        educationActive = entry.isIntersecting;
+                        break;
+                    case 'interests':
+                        setActiveInterests(entry.isIntersecting);
+                        break;
+                }
+            });
+        }
+        const observer = new IntersectionObserver(ioCallback, ioOptions);
+        observer.observe(document.querySelector('#about'));
+        observer.observe(document.querySelector('#skills'));
+        observer.observe(document.querySelector('#education'));
+        observer.observe(document.querySelector('#interests'));
+        const projectObserver 
+            = new IntersectionObserver(ioCallback, ioOptionsForProjectEl);
+        projectObserver.observe(document.querySelector('#projects'));
+    }, []);
+
+
+    
     return (
         <div className={`bg-brownish text-salmonish flex flex-col 
         justify-center gap-6 px-3 pb-3 left-0 top-14 w-screen transform
@@ -12,20 +74,25 @@ const ResponsiveSidebar = ({showSidebar}) => {
             <nav>
                 <ul className="uppercase font-sans space-y-5 font-semibold 
                 lg:space-y-6 lg:text-center">
-                    <li className="hover:text-brighter-salmon">
-                        <a href="#about">About</a>
+                    <li className={`hover:text-brighter-salmon ${activeAbout ? 'text-white' : ''}`}>
+                        <Link to="about" smooth={true} duration={500} delay={350} 
+                        isDynamic={true}>About</Link>
                     </li>
-                    <li className="hover:text-brighter-salmon">
-                        <a href="#skills">Skills</a>
+                    <li className={`hover:text-brighter-salmon ${activeSkills ? 'text-white' : ''}`}>
+                        <Link to="skills" smooth={true} duration={500} delay={350} 
+                        isDynamic={true}>Skills</Link>
                     </li>
-                    <li className="hover:text-brighter-salmon">
-                        <a href="#projects">Projects</a>
+                    <li className={`hover:text-brighter-salmon ${activeProjects ? 'text-white' : ''}`}>
+                        <Link to="projects" smooth={true} duration={500} delay={350} 
+                        isDynamic={true}>Projects</Link>
                     </li>
-                    <li className="hover:text-brighter-salmon">
-                        <a href="#education">Education</a>
+                    <li className={`hover:text-brighter-salmon ${activeEducation ? 'text-white' : ''}`}>
+                        <Link to="education" smooth={true} duration={500} delay={350} 
+                        isDynamic={true}>Education</Link>
                     </li>
-                    <li className="hover:text-brighter-salmon">
-                        <a href="#interests">Interests</a>
+                    <li className={`hover:text-brighter-salmon ${activeInterests ? 'text-white' : ''}`}>
+                        <Link to="interests" smooth={true} duration={500} delay={350} 
+                        isDynamic={true}>Interests</Link>
                     </li>
                 </ul>
             </nav>
