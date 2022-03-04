f8bfb7c22579653952490dd10f1959a68c06e6fb
Matt Di Micelli <matthewdimicelli@gmail.com>
2022-03-03 22:48:19 -0500

    remove word some from about page

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ const About = () => {
                 <section className="text-slate-500 mt-12">
                 I’m a self-taught Full Stack Web Developer with a strong work ethic and a lifelong 
                 passion for learning and technology.  I coupled the open-source Odin Project web 
-                development curriculum with some techniques that I previously used to master the 
+                development curriculum with techniques that I previously used to master the 
                 Spanish language when learning to code from February 2021 to the present.  Although 
                 I’m now experienced with the MERN stack, I made sure to put a major emphasis on 
                 learning the basics first, which include a strong foundation in the three core web 
